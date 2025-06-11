83a4ba9e440fb551312532741b788d7b995bde42
jlhMace <julian.l.mace@gmail.com>
2025-06-11 13:55:01 -0500

    now reading from csv files

--- nn-graph.py
+++ nn-graph.py
@@ -16,24 +16,12 @@ def plot_graph():
     pass
 
 def main():
-    ## Data to CSV
-    df_bins = pd.DataFrame([[1350,27,0.01826039599836804,54.412598416995024,54.43223269599548],
-                        [3200,64,0.04716818800079636,123.822724379992,123.8713450840005],
-                        [6250,125,0.07624013899476267,238.63210353199975,238.7100271179952],
-                        [10800,216,0.13078330700227525,455.56458601700433,455.6973391920037],
-                        [17150,343,0.20886128500569612,707.5550146750029,707.766414194004]],
-                        columns=['atoms','bins','Bin sort','Nearest neighbor','Total elapsed time'])
-    csv_data = df_bins.to_csv('data.csv', index = False) 
-
-    df_less = pd.DataFrame([[1350,49.903866401000414],
-                            [3200,272.63898685600725],
-                            [6250,970.8816703689954],
-                            [10800,3178.949861598012],
-                            [17150,7069.98548579801]],
-                            columns=['atoms','Total elapsed time'])
+    ## CSV to DataFrame
+    df_bins = pd.read_csv('data-graphing/data_spring25_bins.csv',header=0)
+    df_nobins = pd.read_csv('data-graphing/data_spring25_nobins.csv',header=0)
 
     ## Polyfit
-    coefficients = np.polyfit(df_less['atoms'], df_less['Total elapsed time'], 3)
+    coefficients = np.polyfit(df_nobins['atoms'], df_nobins['Total elapsed time'], 3)
     pl = np.poly1d(coefficients)
 
     ## Plot graphs
@@ -42,9 +30,9 @@ def main():
     #slope, intercept, r, p, sterr = scipy.stats.linregress(x=bins_total.get_lines()[0].get_xdata(),y=bins_total.get_lines()[0].get_ydata())
     #plt.text(12000,100, 'y = ' + str(round(intercept,3)) + ' + ' + str(round(slope,3)) + 'x')
 
-    #less_total = sns.scatterplot(data=df_less,x='atoms', y='Total elapsed time',label='Without bin sort total time')
-    #sns.regplot(data=df_less, x='atoms', y='Total elapsed time',line_kws={"color": "blue"},order=2)
-    #plt.plot(df_less['atoms'], pl(df_less['atoms']), label='Quadratic Fit', color='blue')
+    #less_total = sns.scatterplot(data=df_nobins,x='atoms', y='Total elapsed time',label='Without bin sort total time')
+    #sns.regplot(data=df_nobins, x='atoms', y='Total elapsed time',line_kws={"color": "blue"},order=2)
+    #plt.plot(df_nobins['atoms'], pl(df_nobins['atoms']), label='Quadratic Fit', color='blue')
 
     ## Plot bin sort times
     bins_sort = sns.scatterplot(data=df_bins, x='atoms', y='Bin sort', color='orange')
