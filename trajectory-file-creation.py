3acaf6d9401478338095f13d5c826cc5fe0b51ce
jlhMace <julian.l.mace@gmail.com>
2025-06-12 14:26:23 -0500

    now allows traj files with triclinic unit cells

--- trajectory-file-creation.py
+++ trajectory-file-creation.py
@@ -1,42 +1,51 @@
-#### Creates a trajectory file containing 100000 Atom objects
-#### Input: none
-#### Output: .traj file at ~/Documents/code-projects/pyamff/{file-name}.traj
-####    containing 100,000 Atom objects at randomized 3D coordinates
-#### in an orthorhombic unit cell of side 100 Angstroms
+#### Function to create a trajectory file of uniformly distributed atoms
+####    using input unit cell dimensions
 
 from ase.io.trajectory import Trajectory
-from ase.io import write
 from ase import Atoms, Atom
 from random import uniform
+import numpy as np
+from math import cos,sin,sqrt
 
-# set parameters
-cutoff = 10
-element = 'H'
-n = 7           # bins per dimension
-a = n*cutoff    # dimensions of unit cell
-b = n*cutoff
-c = n*cutoff
-num = pow(n,3)*50    # number of total atoms
-print(num,pow(n,3),num/pow(n,3))
 
-# creates list of Atoms objects
-atom_list = Atoms(cell=[a,b,c])
-for e in range(num):
-    x = uniform(0,a)
-    y = uniform(0,b)
-    z = uniform(0,c)
-    atom_list.append(Atom(element,position=(x,y,z)))
+def create_trajectory(filename,cutoff,a,b,c,alpha=90,beta=90,gamma=90):
+    '''Create 1-step trajectory test file of uniformly distributed hydrogen atoms
+    Input:  a,b,c               dimensions of unit cell
+            alpha,beta,gamma    angles of unit cell
+    Output: {filename}.traj     Trajectory file created at {filename}.traj'''
 
-# Configures traj file
-traj = Trajectory('trajprop-10Acutoff-70A.traj', 'w', atoms=atom_list)
-traj.write(atom_list)
-traj.close()
+    # parameters and density check
+    element = 'H'
+    num = int((a*b*c)*50/pow(cutoff,3))    # number of total atoms
+    bins = int((a*b*c)/pow(cutoff,3))
+    print(num,bins,num/bins)
 
-'''
-# Test print out atom_list
-traj = Trajectory('10000atoms-100A.traj')
-for atoms in traj:
-    for atom in atoms:
-        print(atom.position)
-traj.close()
-'''
+    # Transformation matrix
+    cosalp = (cos(beta)*cos(gamma)-cos(alpha))/(sin(beta)*sin(gamma))
+    A = np.array([[a,b*cos(gamma),c*cos(beta)],
+                [0,b*sin(gamma),-c*sin(beta)*cosalp],
+                [0,0,c*sin(beta)*sqrt(1-pow(cosalp,2))]])
+
+    # creates list of Atoms objects
+    atom_list = Atoms(cell=[a,b,c,alpha,beta,gamma])
+    for e in range(num):
+        # generates uniform fractional coordinates
+        x = uniform(0,1)
+        y = uniform(0,1)
+        z = uniform(0,1)
+        # Each point is transformed from fractional to orthogonal coordinates
+        ortho_coord = np.matmul(A,np.array([x,y,z]))
+        atom_list.append(Atom(element,position=ortho_coord))
+
+    # Configures traj file
+    traj = Trajectory(f'{filename}', 'w', atoms=atom_list)
+    traj.write(atom_list)
+    traj.close()
+
+def main():
+    #create_trajectory('test2',10,100,100,100,90,90,70)
+    pass
+
+
+if __name__=='__main__':
+    main()
