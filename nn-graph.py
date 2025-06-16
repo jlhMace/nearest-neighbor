3d13988a0ff6bc4b25a0cfd36ec6f4b413189e89
jlhMace <julian.l.mace@gmail.com>
2025-06-16 15:47:38 -0500

    cleared out extraneous plotting functions

--- nn-graph.py
+++ nn-graph.py
@@ -17,34 +17,30 @@ def plot_graph():
 
 def main():
     ## CSV to DataFrame
-    df_bins = pd.read_csv('data-graphing/data_spring25_bins.csv',header=0)
-    df_nobins = pd.read_csv('data-graphing/data_spring25_nobins.csv',header=0)
+    df = pd.read_csv('data-graphing/orthorhombic_thin.csv',header=0)
+    df_nb = pd.read_csv('data-graphing/orthorhombic_thin_nobin.csv',header=0)
 
-    ## Polyfit
-    coefficients = np.polyfit(df_nobins['atoms'], df_nobins['Total elapsed time'], 3)
-    pl = np.poly1d(coefficients)
+    ## Plot graphs for bins
+    df_one = df[df["width"]==1]
+    df_two = df[df["width"]==2]
+    bins_one = sns.regplot(data=df_one,x='atoms', y='Total elapsed time',label='10A thick, bins',color="blue")
+    bins_two = sns.regplot(data=df_two,x='atoms', y='Total elapsed time',label='20A thick, bins',color="green")
 
-    ## Plot graphs
-    #bins_total = sns.scatterplot(data=df_bins,x='atoms', y='Total elapsed time',label='Bin sort total time')
-    #sns.regplot(data=df_bins, x='atoms', y='Total elapsed time',line_kws={"color": "purple"})
-    #slope, intercept, r, p, sterr = scipy.stats.linregress(x=bins_total.get_lines()[0].get_xdata(),y=bins_total.get_lines()[0].get_ydata())
+    ## Plot graphs for no bins
+    df_nb_one = df_nb[df["width"]==1]
+    df_nb_two = df_nb[df["width"]==2]
+    bins_nb_one = sns.regplot(data=df_nb_one,x='atoms', y='Total elapsed time',label='10A thick, no bins',color="red",order=2)
+    bins_nb_two = sns.regplot(data=df_nb_two,x='atoms', y='Total elapsed time',label='20A thick, no bins',color='orange',order=2)
+
+    ## For regression formula
+    #slope, intercept, r, p, sterr = scipy.stats.linregress(x=df_one.get_lines()[0].get_xdata(),y=df_one.get_lines()[0].get_ydata())
     #plt.text(12000,100, 'y = ' + str(round(intercept,3)) + ' + ' + str(round(slope,3)) + 'x')
-
-    #less_total = sns.scatterplot(data=df_nobins,x='atoms', y='Total elapsed time',label='Without bin sort total time')
-    #sns.regplot(data=df_nobins, x='atoms', y='Total elapsed time',line_kws={"color": "blue"},order=2)
-    #plt.plot(df_nobins['atoms'], pl(df_nobins['atoms']), label='Quadratic Fit', color='blue')
-
-    ## Plot bin sort times
-    bins_sort = sns.scatterplot(data=df_bins, x='atoms', y='Bin sort', color='orange')
-    bins_sort_reg = sns.regplot(data=df_bins, x='atoms', y='Bin sort',line_kws={"color": "purple"})
-    slope, intercept, r, p, sterr = scipy.stats.linregress(x=bins_sort_reg.get_lines()[0].get_xdata(),y=bins_sort_reg.get_lines()[0].get_ydata())
-    plt.text(12000,0.05, 'y = ' + str(round(intercept,3)) + ' + ' + str(round(slope,3)) + 'x')
 
     plt.ylabel('Total time (s)')
     plt.xlabel('Number of atoms')
-    plt.title('Computational Cost of Bin Sort')
-    #plt.legend()
-    plt.show()
+    plt.title('Computational Cost of Thin System')
+    plt.legend()
+    plt.savefig('data-graphing/orthorhombic_thin.png')
         
 
 if __name__=='__main__':
