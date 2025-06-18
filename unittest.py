9f83edae96ba2c6a46b1dd4b1773d26600ce63e7
jlhMace <julian.l.mace@gmail.com>
2025-06-18 14:24:56 -0500

    added unittest for reference traj/txt file existance

---
+++ unittest.py
@@ -0,0 +1,51 @@
+import unittest
+import io
+import os
+#import nn_main as nn
+import re
+
+
+class TestReferenceFiles():
+
+    def __init__(self):
+        self.directory = 'data-testing'
+    
+    def file_list(self):
+        filelist = []
+        for file in os.listdir(self.directory):
+            filename = os.fsdecode(file)
+            if filename.endswith('.traj'):
+                filelist.append(filename)
+        return filelist
+    
+    def check_file_exists(self,filename):
+        filename = re.sub('.traj','.txt',filename)
+        file = str(self.directory + '/' + filename)
+        with io.open(file) as f:
+            assert os.path.isfile(file)
+
+    def test_gen_file_exists(self):
+        filelist = self.file_list()
+        for e in filelist:
+            yield self.check_file_exists, e
+
+
+class TestBinAccuracy(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    @unittest.skip("Base test case")
+    def test_nn_base(self,test_path,ref_path):
+        with io.open(test_path) as tp:
+            with io.open(ref_path) as rp:
+                self.assertEqual(list(tp),list(rp))
+
+    def test_nn_cubic(self):
+            #nn.run_nn()
+            self.test_nn_base()
+            
+
+
+if __name__=='__main__':
+    unittest.main()
