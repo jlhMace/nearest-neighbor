846ee63ff97faa326200bcde4322e9968f7fbcba
jlhMace <julian.l.mace@gmail.com>
2025-06-20 15:19:05 -0500

    updates unittests to .pkl output

--- unittests.py
+++ unittests.py
@@ -4,7 +4,7 @@ import unittest
 #import nn_main as nn
 from ase.io import Trajectory
 import re
-from comparetest import create_neighborlist
+from comparetest import create_neighborlist, create_reference, compare_files
 
 
 class TestReferenceFiles():
@@ -21,7 +21,7 @@ class TestReferenceFiles():
         return filelist
     
     def check_file_exists(self,filename):
-        filename = re.sub('.traj','.json',filename)
+        filename = re.sub('.traj','.pkl',filename)
         file = str(self.directory + '/' + filename)
         with io.open(file) as f:
             assert os.path.isfile(file)
@@ -45,24 +45,23 @@ class TestBinAccuracy():
                 filelist.append(filename)
         return filelist
     
-    @unittest.skip('Work in progress, remove skip when nn_main is fixed.')
+    #@unittest.skip('Work in progress, remove skip when nn_main is fixed.')
     def setUp(self):
         filelist = self.file_list()
         for trajfile in filelist:
             trajfile = str(self.directory + '/' + trajfile)
-            outfile = re.sub('.traj','.json',trajfile)
+            outfile = re.sub('.traj','_test.pkl',trajfile)
             create_neighborlist(trajfile,outfile)
 
-    def compare_output(self,filename):
-        filename = re.sub('.traj','.json',filename)
-        file = str(self.directory + '/' + filename)
-        with io.open(file) as f:
-            assert os.path.isfile(file)
+    def compare_output(self,f1,f2):
+        compare_files(f1,f2)
 
     def test_gen_file_compare(self):
         filelist = self.file_list()
         for e in filelist:
-            yield self.compare_output, e
+            f1 = re.sub('.traj','.pkl',e)
+            f2 = re.sub('.pkl','_test.pkl',e)
+            yield self.compare_output, f1, f2
             
 
 
